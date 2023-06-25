93316f5d9b8cd525ee7cb7b55d68fe63a7c67d6e
Jakku Night <J97k107u45k117n@gmail.com>
2023-06-25 16:20:15 -0400

    Added support for GitHUb Pages.

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { base } from "$app/paths";
+</script>
+<style>
+    div{
+        background-color: darkslategray;
+        height: auto;
+        width: 100%;
+        color: aquamarine;
+        margin: 0px;
+        padding: 5%;
+    }
+    a{
+        text-decoration: none;
+    }
+</style>
+<div>
+    <a href="{base}/">
+        <button>
+            Home
+        </button>
+    </a>
+        <a href="{base}/blog">
+        <button>
+            Blog
+        </button>
+    </a>
+        <a href="{base}/about">
+        <button>
+            Acerca de...
+        </button>
+    </a>
+</div>
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,9 @@
+<h1>
+    Blog
+</h1>
+<h3>
+    Bienvenido a mi Blog personal!
+</h3>
+<p>
+    Aqui inicia el blog.
+</p>
